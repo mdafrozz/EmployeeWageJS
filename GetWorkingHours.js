8efeeb2b113d4818ff32615cd77332bff14df074
mdafrozz <106298595+mdafrozz@users.noreply.github.com>
2022-08-16 09:01:30 +0530

    Refactor: Calculate total employee wage

--- GetWorkingHours.js
+++ GetWorkingHours.js
@@ -7,6 +7,7 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160;
 
 function getWorkingHours(empCheck) {
     switch (empCheck) {
@@ -20,18 +21,14 @@ function getWorkingHours(empCheck) {
 }
 
 /*
- * UC4 Calculate monthly wages
+ * UC5 Calculate monthly wages still total working days
  */
- let totalEmpHrs = 0;
- for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
-     let empCheck = Math.floor(Math.random() * 10) % 3;
-     totalEmpHrs += getWorkingHours(empCheck);
- }
- 
-// let empCheck = Math.floor(Math.random() * 10) % 3;
-// let empHrs = getWorkingHours(empCheck);
-// let empWage = empHrs * WAGE_PER_HOUR;
-// console.log(" Emp Hour:" + empHrs + " Emp Wage: " + empWage);
-
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    totalEmpHrs += getWorkingHours(empCheck);
+}
 let empWage = totalEmpHrs * WAGE_PER_HOUR;
-console.log("Total Hours:" + totalEmpHrs + " Employee Wage:" + empWage);
+console.log("Total Days:" + totalWorkingDays + " Total Hours:" + totalEmpHrs + " Employee Wage:" + empWage);
